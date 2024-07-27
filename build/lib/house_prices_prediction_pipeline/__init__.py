720d7a1a886bc23c23b9630b2c70df96bb203e70
Saad Khalid <saad.khalid@inboxhealth.com>
2024-07-27 13:45:07 +0500

    removed build directories

--- build/lib/house_prices_prediction_pipeline/__init__.py
+++
@@ -1,8 +0,0 @@
-FEATURES = ['price', 'area', 'bedrooms', 'bathrooms', 'stories',
-            'mainroad', 'guestroom', 'basement', 'hotwaterheating',
-            'airconditioning', 'parking', 'prefarea',
-            'furnishingstatus']
-CONTINIOUS_FEATURES = ['area', 'bedrooms']
-CATEGORICAL_FEATURES = ['mainroad', 'furnishingstatus']
-TARGET_FEATURE = 'price'
-OUTPUT_DIR = '../models/'
